bc8220d201fc1d4d67d14675b788e4d956c6abd3
Ash-JMC <57333508+Ash-JMC@users.noreply.github.com>
2020-12-13 23:47:49 +1100

    1

---
+++ SeekerMissiles/Assets/Scripts/RotTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotTest : MonoBehaviour
+{
+    public float angle;
+    public Vector3 targetRot;
+    public float speed;
+    private bool on;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && !on)
+        {
+            on = true;
+        }
+
+        if(on)
+        {
+            Quaternion lookRot = Quaternion.LookRotation(targetRot);
+            Quaternion stepRot = Quaternion.RotateTowards(transform.rotation, lookRot, speed * Time.deltaTime);
+            transform.rotation = stepRot;
+        }
+    }
+
+}
