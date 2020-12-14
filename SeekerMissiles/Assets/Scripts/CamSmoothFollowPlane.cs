b8346f3f29f5da69135eb84782f4e055f5302418
Ash-JMC <57333508+Ash-JMC@users.noreply.github.com>
2020-12-15 00:33:43 +1100

    2

---
+++ SeekerMissiles/Assets/Scripts/CamSmoothFollowPlane.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class CamSmoothFollowPlane : MonoBehaviour
+{
+    
+    public float followSpeed, alignSpeed;
+    public bool lerpFollow;
+    public float lerpBias;
+    public float pitchBias;
+    public float maxPitch;
+
+    public Transform target;
+    private int flip;
+
+    void Start()
+    {
+        target = transform.parent;
+        transform.parent = null;
+    }
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.C))
+        {
+            flip = flip == 1 ? -1 : 1;
+            transform.Rotate(Vector3.up * 90f * flip);
+        }
+
+        
+
+    }
+    void FixedUpdate()
+    {
+        Vector3 targetPos = lerpFollow ? Vector3.Slerp(transform.position, target.position, lerpBias) : Vector3.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+        transform.position = targetPos;
+
+        Quaternion targetRot = Quaternion.LookRotation( target.forward, Vector3.up);
+        Quaternion easedRot = Quaternion.Euler(targetRot.eulerAngles.x, targetRot.eulerAngles.y, 0);
+        print(targetRot.eulerAngles.x);
+        Quaternion stepRot = Quaternion.RotateTowards(transform.rotation, easedRot, alignSpeed * Time.deltaTime);
+        transform.rotation = stepRot;
+        //Mathf.LerpAngle(transform.rotation.x, targetRot.x, pitchBias)
+    }
+}
