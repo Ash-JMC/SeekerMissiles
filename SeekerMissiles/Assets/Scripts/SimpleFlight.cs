b9ae8df58f8b2f35c8cd37dc4f1a0b9ee480ff54
Ash-JMC <ahall1@jmc.edu.au>
2021-01-22 15:11:02 +1100

    Pew pew

--- SeekerMissiles/Assets/Scripts/SimpleFlight.cs
+++ SeekerMissiles/Assets/Scripts/SimpleFlight.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SimpleFlight : MonoBehaviour
 {
-    public float thrust, maxThrust, pitchSpeed, yawSpeed, rollSpeed, gravModNoLift, liftAngleMax, dragAngle;
+    public float thrust, maxThrust, pitchSpeed, yawSpeed, rollSpeed, gravModNoLift, liftAngleMax, dragAngle, rollMod;
     [Range(0f, 1f)]
     public float alignVelocity;
     public Rigidbody rb;
@@ -27,7 +27,7 @@ public class SimpleFlight : MonoBehaviour
         float velAngle = Vector3.Angle(rb.velocity, transform.forward);
         float turnDrag = Mathf.Max(0.1f, 1 - velAngle / dragAngle);
         print(turnDrag);
-        Vector3 turn = new Vector3(pitch * turnDrag, yaw * turnDrag, roll * turnDrag) * Time.deltaTime;
+        Vector3 turn = new Vector3(pitch * turnDrag, yaw * turnDrag, roll * Mathf.Min(turnDrag * 2, 1)) * Time.deltaTime;
         transform.Rotate(turn);
             
     }
@@ -47,7 +47,7 @@ public class SimpleFlight : MonoBehaviour
         Vector3 vel = Vector3.Lerp(rb.velocity, transform.forward * thrust * cThrust * boostMod, alignVelocity * Time.fixedDeltaTime);
         vel.y -= 9.8f * Time.fixedDeltaTime;
         rb.velocity = vel;
-        rb.AddForce(Vector3.up * -9.81f * Time.fixedDeltaTime * (liftMod * liftMod2) + ((transform.right + transform.up * 0.1f) * -Input.GetAxis("Roll") * rollSpeed * 3.5f) * Mathf.Max(Input.GetAxis("Thrust"), 0), ForceMode.Impulse);
+        rb.AddForce(Vector3.up * -9.81f * Time.fixedDeltaTime * (liftMod * liftMod2) + (transform.right * -Input.GetAxis("Roll") * rollSpeed * rollMod) * Mathf.Max(Input.GetAxis("Thrust"), 0), ForceMode.Impulse);
         // max forward vs max total etc
 
     }
